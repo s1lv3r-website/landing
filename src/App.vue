eb8d28f8e14f2216bdcd387613db79d190abb929
ent3r <32072697+ent3r@users.noreply.github.com>
2020-09-12 15:43:02 +0200

    :recycle: Split up code into individual chunks

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <AboutMe />
+  <div id="container">
+    <AboutMe />
+    <br />
+    <SocialMedia />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import AboutMe from "./components/AboutMe.vue";
+import Social from "./components/Social.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    AboutMe: AboutMe
+    AboutMe: AboutMe,
+    SocialMedia: Social
   }
 });
 </script>
@@ -21,5 +27,19 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+</style>
+
+<style lang="scss" scoped>
+#container {
+  max-width: 350px;
+  border: 1px currentColor solid;
+  border-radius: 5px;
+  padding: 10px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
 }
 </style>
