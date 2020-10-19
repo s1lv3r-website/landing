5a0ee5441861f539d36c826b5a739e668249d108
ent3r <32072697+ent3r@users.noreply.github.com>
2020-10-19 22:23:51 +0200

    :arrow_down: Downgrade to vue 2, v3 isnt stable enough yet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { createApp } from "vue";
+import VueMeta from "vue-meta";
+
+const test = new VueMeta()
+
 import App from "./App.vue";
 import "./registerServiceWorker";
 
-createApp(App).mount("#app");
+createApp(App).use(test, {}).mount("#app");
