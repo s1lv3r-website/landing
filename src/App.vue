45fd52e939591597281bab51a69c3a6d6609da2d
ent3r <32072697+ent3r@users.noreply.github.com>
2020-10-20 15:32:21 +0200

    :sparkles: Meta tags?

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,27 @@
 <template>
-  <div id="container">
-    <AboutMe />
-    <br />
-    <SocialMedia />
+  <div id="app">
+    <div id="nav"></div>
+    <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import AboutMe from "./components/AboutMe.vue";
-import Social from "./components/Social.vue";
 
 export default Vue.extend({
   name: "App",
-  components: {
-    AboutMe: AboutMe,
-    SocialMedia: Social
-  },
-  created() {
-    document.title = "S1LV3R";
+  metaInfo: {
+    title: "S1LV3R"
   }
 });
 </script>
 
 <style lang="scss">
-body {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-</style>
-
-<style lang="scss" scoped>
-#container {
-  max-width: 350px;
-  border: 1px currentColor solid;
-  border-radius: 5px;
-  padding: 10px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import Vue from "vue";
+import VueMeta from "vue-meta";
+
 import App from "./App.vue";
 import "./registerServiceWorker";
+import router from "./router";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueMeta);
+
 new Vue({
-  render: h => h(App)
+  router,
+  render: (h) => h(App),
 }).$mount("#app");
