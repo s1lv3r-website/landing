311286844318566623e5f2a33a52348fca0d41aa
ent3r <32072697+ent3r@users.noreply.github.com>
2020-10-30 20:45:26 +0100

    :art: Round pfp nice

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,7 +3,8 @@
     <img
       src="https://gravatar.com/avatar/708126e718f81d89e6daffba2b153c72.png?size=200&d=retro"
       alt="My logo"
-    />
+      id="logo"
+/>
     <h1 id="username">S1LV3R</h1>
     <p
       id="description"
@@ -23,5 +24,9 @@ export default Vue.extend({
 <style scoped lang="scss">
 h3 {
   margin: 30px 0 0;
+}
+
+#logo {
+  border-radius: 50%;
 }
 </style>
