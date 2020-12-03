cf188bc66ee1cdde37bb2c6cbb6aa2d4bbffaaeb
theS1LV3R <32072697+ent3r@users.noreply.github.com>
2020-12-03 21:41:05 +0100

    fix: misc 404 fixes

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,3 +1,12 @@
 <template>
     <h1>404. That page wasn't found. Nice job fam</h1>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    metaInfo: {
+        title: "404 Not Found"
+    }
+})
+</script>
