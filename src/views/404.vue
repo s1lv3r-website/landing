31e2af59b1326fa8152121983ebfe2f7f0f09bde
theS1LV3R <32072697+ent3r@users.noreply.github.com>
2020-12-03 21:56:50 +0100

    style: formatting

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,12 +1,12 @@
 <template>
-    <h1>404. That page wasn't found. Nice job fam</h1>
+  <h1>404. That page wasn't found. Nice job fam</h1>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
-    metaInfo: {
-        title: "404 Not Found"
-    }
-})
+  metaInfo: {
+    title: "404 Not Found",
+  },
+});
 </script>
