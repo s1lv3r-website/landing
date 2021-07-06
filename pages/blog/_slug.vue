47384e90debfc593681a0720ff03f986db8d7fd7
S1LV3R <s1lv3r@corax.team>
2021-07-07 01:12:03 +0200

    feat: add back button and title to blog pages

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,16 +1,29 @@
 <template>
-  <article>
-    <nuxt-content :document="article" />
-  </article>
+  <div>
+    <NuxtLink :to="previousLink"><Octicon icon="arrow-left" /> Back</NuxtLink>
+    <article>
+      <h1>{{ article.title }}</h1>
+      <nuxt-content :document="article" />
+    </article>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+
+import Octicon from '~/components/octicon.vue'
+
 export default Vue.extend({
+  components: { Octicon },
+
   async asyncData({ $content, params }) {
     const article = await $content('blog', params.slug).fetch()
 
     return { article }
+  },
+
+  data() {
+    return { previousLink: '/blog' }
   },
 })
 </script>
