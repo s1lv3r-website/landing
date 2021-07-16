79a1fa07296837d96b460a8e05a4fecd9480859a
S1LV3R <s1lv3r@corax.team>
2021-07-16 16:48:30 +0200

    feat: update how blog posts are styled and listed

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -11,21 +11,37 @@
         {{ tag }}
       </p>
     </div>
+
     <ul>
-      <li v-for="article in filteredArticles" :key="article.url">
-        <h2 class="blog-title">
-          <NuxtLink :to="`/blog/${article.slug}`">{{ article.title }}</NuxtLink>
+      <li v-for="monthYear in articlesByMonthYear" :key="monthYear.monthYear">
+        <h2>
+          <span class="month-year--month">{{ monthYear.month }}</span>
+          <span class="month-year--year">- {{ monthYear.year }}</span>
         </h2>
-        <p class="blog-meta">
-          {{ formatDate(article.createdAt) }}
-          <span v-if="lastUpdatedDiffers(article)">
-            | Updated on {{ formatDate(article.updatedAt) }}
-          </span>
-          <span v-if="Array.isArray(article.tags)">
-            |
-            {{ article.tags.map((tag) => tag.toLowerCase()).join(', ') }}</span
-          >
-        </p>
+        <hr />
+        <ul>
+          <li v-for="article in monthYear.articles" :key="article.url">
+            <h2 class="blog-title">
+              <NuxtLink :to="`/blog/${article.slug}`">{{
+                article.title
+              }}</NuxtLink>
+            </h2>
+            <p class="blog-meta">
+              {{ formatDate(article.date) }}
+              <span v-if="lastUpdatedDiffers(article)">
+                | Updated on {{ formatDate(article.date) }}
+              </span>
+              <span
+                v-if="Array.isArray(article.tags) && article.tags.length > 0"
+              >
+                |
+                {{
+                  article.tags.map((tag) => tag.toLowerCase()).join(', ')
+                }}</span
+              >
+            </p>
+          </li>
+        </ul>
       </li>
     </ul>
   </div>
@@ -34,6 +50,36 @@
 
 <script lang="ts">
 import Vue from 'vue'
+
+interface IContentDocument extends Record<string, any> {
+  dir: string
+  path: string
+  extension: '.md' | '.json' | '.yaml' | '.xml' | '.csv' | string
+  slug: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+interface BlogPost extends IContentDocument {
+  title: string
+  date: string
+  tags?: Array<string>
+}
+
+const MONTHS: { [key: string]: number } = {
+  JANUARY: 0,
+  FEBRUARY: 1,
+  MARCH: 2,
+  APRIL: 3,
+  MAY: 4,
+  JUNE: 5,
+  JULY: 6,
+  AUGUST: 7,
+  SEPTEMBER: 8,
+  OCTOBER: 9,
+  NOVEMBER: 10,
+  DECEMBER: 11,
+}
 
 export default Vue.extend({
   async asyncData({ $content }) {
@@ -64,7 +110,7 @@ export default Vue.extend({
   },
 
   computed: {
-    filteredArticles() {
+    filteredArticles(): Array<BlogPost> {
       if (this.activeTag !== '') {
         // @ts-ignore
         return this.articles.filter((article) =>
@@ -77,12 +123,62 @@ export default Vue.extend({
         return this.articles
       }
     },
+    articlesByMonthYear() {
+      const articlesByMonth: {
+        month: string
+        year: number
+        articles: BlogPost[]
+      }[] = []
+
+      this.filteredArticles.forEach((article) => {
+        const date = new Date(article.date)
+        const year = date.getFullYear()
+        const month = date
+          .toLocaleDateString('en-US', { month: 'long' })
+          .toLowerCase()
+
+        const monthArticles = articlesByMonth.find(
+          (articleByMonth) =>
+            articleByMonth.month === month && articleByMonth.year === year
+        )
+
+        if (monthArticles) {
+          monthArticles.articles.push(article)
+        } else {
+          articlesByMonth.push({
+            month,
+            year,
+            articles: [article],
+          })
+        }
+      })
+
+      articlesByMonth.sort((a, b) => {
+        const { month: aMonth, year: aYear } = a
+        const { month: bMonth, year: bYear } = b
+
+        const aMonthIndex = MONTHS[aMonth.toUpperCase()]
+        const bMonthIndex = MONTHS[bMonth.toUpperCase()]
+
+        if (aYear < bYear) return 1
+
+        if (aMonthIndex > bMonthIndex) return 1
+
+        return 0
+      })
+
+      articlesByMonth.forEach((month) => {
+        month.articles.sort((a, b) => (a.date > b.date ? 0 : 1))
+      })
+
+      return articlesByMonth
+    },
   },
 
   methods: {
     // @ts-ignore
     lastUpdatedDiffers(article) {
-      return article.createdAt !== article.updatedAt
+      return article.date !== article.updatedAt
     },
     formatDate(dateString: string) {
       return new Date(dateString).toLocaleDateString('en-US', {
@@ -119,10 +215,22 @@ html {
     .blog-meta {
       color: $lightFooterTextColor;
     }
+
+    .month-year {
+      &--year {
+        color: $darkFooterTextColor;
+      }
+    }
   }
   &.dark-mode {
     .blog-meta {
       color: $darkFooterTextColor;
+    }
+
+    .month-year {
+      &--year {
+        color: $lightFooterTextColor;
+      }
     }
   }
 }
@@ -157,5 +265,11 @@ ul {
 
 .wrapper {
   margin-top: 1rem;
+}
+
+.month-year {
+  &--year {
+    font-size: 1.25rem;
+  }
 }
 </style>
