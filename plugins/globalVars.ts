3be335f42864f6b20c617efd2baa963300df0b34
S1LV3R <s1lv3r@corax.team>
2021-07-19 00:01:28 +0200

    chore(types): add typings to plugin

--- plugins/globalVars.ts
+++ plugins/globalVars.ts
@@ -1,15 +1,23 @@
 import { Plugin } from '@nuxt/types'
 
+const vars = {
+  email: 's1lv3r@corax.team',
+  github_username: 'theS1LV3R',
+  twitter_username: 'theS1LV3R',
+  discord_username: 'Join my server',
+  matrix_username: '@s1lv3r:matrix.org',
+  repo: 's1lv3r-website/landing',
+  branch: 'master',
+}
+
+declare module '@nuxt/types' {
+  interface Context {
+    $globalVars: typeof vars
+  }
+}
+
 const pluginFunction: Plugin = (_, inject) => {
-  inject('globalVars', {
-    email: 's1lv3r@corax.team',
-    github_username: 'theS1LV3R',
-    twitter_username: 'theS1LV3R',
-    discord_username: 'Join my server',
-    matrix_username: '@s1lv3r:matrix.org',
-    repo: 's1lv3r-website/landing',
-    branch: 'master',
-  })
+  inject('globalVars', vars)
 }
 
 export default pluginFunction
